2ad107441e504f575fc4d7961e2a5e2320e6a06b
NedelescuVlad <nedelescu.vlad@gmail.com>
2016-10-20 20:43:58 +0000

    Mockup a grid based layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
+
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(600);
+
+        primaryStage.setTitle("Minesweeper");
+
         primaryStage.show();
     }
 
